250b576516f8864f4d6329ead8e6bee7d697961e
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-11-02 09:46:25 +0530

    update
    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './master.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div>
+          <h1>Join our community</h1>
+
+          <h2>30-day, hassle-free money back guarantee</h2>
+
+          <p>Gain access to our full library of tutorials along with expert code reviews.
+          Perfect for any developers who are serious about honing their skills.</p>
+        </div>
+
+        <div>
+          <h2>Monthly Subscription</h2>
+
+          <p><span>$29</span> per month</p>
+
+          <p>Full access for less than &dollar;1 a day</p>
+
+          <button>Sign Up</button>
+        </div>
+
+        <div>
+          <h2>Why Us</h2>
+
+          <ul>
+            <li>Tutorials by industry experts</li>
+            <li>Peer & expert code review</li>
+            <li>Coding exercises</li>
+            <li>Access to our GitHub repos</li>
+            <li>Community forum</li>
+            <li>Flashcard decks</li>
+            <li>New videos every week</li>
+          </ul>
+
+        </div>
+      </main>
+      <footer>
+    <p class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Your Name Here</a>.
+    </p>
+  </footer>
     </div>
   );
 }
